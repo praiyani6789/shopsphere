132fad725c926ca8b846a77997c5841af1c92211
praiyani6789 <prince.raiyani10@gmail.com>
2024-08-12 17:05:28 +0530

    upto delete account

---
+++ lib/features/personalization/screens/profile/widgets/change_name.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:shopsphere/common/widgets/appbar/appbar.dart';
+import 'package:shopsphere/features/personalization/controllers/update_name_controller.dart';
+import 'package:shopsphere/utils/constants/sizes.dart';
+import 'package:shopsphere/utils/validator/validation.dart';
+
+import '../../../../utils/constants/text_strings.dart';
+
+class ChangeName extends StatelessWidget {
+  const ChangeName({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(updateNameController());
+    return Scaffold(
+      appBar: SAppBar(
+        showBackArrow: true,
+        title: Text('Change Name',
+            style: Theme.of(context).textTheme.headlineSmall),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(SSizes.defaultSpace),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //Heading
+            Text(
+                'Use real name for easy verification. This name will appear on several pages.',
+                style: Theme.of(context).textTheme.labelMedium),
+            const SizedBox(height: SSizes.spaceBtwSections),
+
+            //Text field and Button
+            Form(
+              key: controller.updateUserNameFormKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: controller.firstName,
+                    validator: (value) =>
+                        SValidator.validateEmptyText('First name', value),
+                    expands: false,
+                    decoration: const InputDecoration(
+                        labelText: STexts.firstName,
+                        prefixIcon: Icon(Iconsax.user)),
+                  ),
+                  const SizedBox(height: SSizes.spaceBtwInputFields),
+                  TextFormField(
+                    controller: controller.lastName,
+                    validator: (value) =>
+                        SValidator.validateEmptyText('Last name', value),
+                    expands: false,
+                    decoration: const InputDecoration(
+                        labelText: STexts.lastName,
+                        prefixIcon: Icon(Iconsax.user)),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: SSizes.spaceBtwSections),
+
+            //Save Button
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () => controller.updateUserName(),
+                  child: Text('Save')),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
